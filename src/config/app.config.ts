93f66bef41cb1746629a536e97d442d072b9e3c8
Oktawn <belyhmitia@gmail.com>
2025-05-03 15:09:05 +0500

    start

---
+++ src/config/app.config.ts
@@ -0,0 +1,36 @@
+import { config } from "dotenv";
+import path from "node:path";
+
+interface IAppConfig {
+  API_PORT: number;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_DATABASE: string;
+  DB_EXTERNAL_PORT: number;
+  DB_PORT: number;
+}
+
+class EnvConfig {
+  constructor() {
+    const envDev = path.resolve(__dirname, "../../.env");
+    const envProd = path.resolve(__dirname, "../../../.env");
+    const envPath = envDev || envProd;
+    const { error, parsed } = config({ path: envPath });
+    if (error) {
+      throw new Error(`Error loading .env file: ${error}`);
+    }
+    if (!parsed) {
+      throw new Error("Error parsing .env file");
+    }
+  }
+
+  public get(key: (keyof IAppConfig)): string {
+    const value = process.env[key];
+    if (!value) {
+      throw new Error(`Missing environment variable: ${key}`);
+    }
+    return value;
+  }
+}
+
+export const envConfig = new EnvConfig();
